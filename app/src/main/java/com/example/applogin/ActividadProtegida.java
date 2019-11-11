d5cb31d0c94661fa17a081a44b48e795db8c7b3d
Iagotero <iago.otero7@gmail.com>
2019-11-11 22:02:12 +0100

    Intentando el login#1 Interfaz completa

---
+++ app/src/main/java/com/example/applogin/ActividadProtegida.java
@@ -0,0 +1,32 @@
+package com.example.applogin;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class ActividadProtegida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_protegida);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/applogin/MainActivity.java
+++ app/src/main/java/com/example/applogin/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.applogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.FileDescriptor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mensaje();
+
+            }
+        });
+    }
+
+    private void mensaje() {
+
+        Intent intent = new Intent(this,ActividadProtegida.class);
+        startActivity(intent);
+
     }
 }
